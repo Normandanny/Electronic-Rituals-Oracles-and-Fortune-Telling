6151f8f9635421f9df30be8607bfb46566787630
Normandanny <NormandannyEOF@gmail.com>
2018-05-03 12:36:58 -0400

    Demo

--- Dream Visualizer/bubble.js
+++ Dream Visualizer/bubble.js
@@ -4,41 +4,11 @@ class bubble {
         this.x = x;
         this.y = y;
         this.selected = false;
-        this.show = true;
+        this.show = false;
     }
 
-    // display() {
-    //     // Draw the bubble
-    //     fill("#D496A7");
-    //     noStroke();
-    //     ellipseMode(CENTER);
-    //     ellipse(this.x, this.y, 100, 100);
-
-    //     // Write the text
-    //     fill(255);
-    //     textAlign(CENTER);
-    //     textSize(18);
-    //     text(this.title, this.x, this.y + 5);
-
-    // }
-
-    display() {
-        if (this.show) {
-            if (this.selected) {
-                // Draw the bubble
-                fill("#D496A7");
-                noStroke();
-                ellipseMode(CENTER);
-                ellipse(this.x, this.y, 100, 100);
-
-                // Write the text
-                fill(0);
-                textAlign(CENTER);
-                textSize(18);
-                text(this.title, this.x, this.y + 5);
-
-            } else {
-                fill("#D496A7");
+    display_selected() {
+                fill("#EA5A7A");
                 noStroke();
                 ellipseMode(CENTER);
                 ellipse(this.x, this.y, 100, 100);
@@ -49,6 +19,19 @@ class bubble {
                 textSize(24);
                 text(this.title, this.x, this.y + 8);
             }
-        }
-    }
+
+    display() {
+           // Draw the bubble
+           fill("#D496A7");
+           noStroke();
+           ellipseMode(CENTER);
+           ellipse(this.x, this.y, 100, 100);
+
+           // Write the text
+           fill(255);
+           textAlign(CENTER);
+           textSize(24);
+           text(this.title, this.x, this.y + 5);
+
+       } 
 }
--- Dream Visualizer/sketch.js
+++ Dream Visualizer/sketch.js
@@ -1,9 +1,11 @@
 // Stage detection
-let choiceStage;
-let visualStage;
+let choiceStage = true;
 
 let wordHolder = [];
 let bubbles = [];
+
+let counter = 0;
+let tr = 0;
 
 // // ES Modules syntax
 // import Unsplash from 'unsplash-js';
@@ -25,46 +27,90 @@ myChoice.interimResults = true; // allow partial recognition (faster, less accur
 function preload() {
     wordHolder = loadJSON("words.json");
 
-    // pkmlogo = loadImage("Assets/International_Pokémon_logo.png");
+    dream = loadImage("assets/cloud.jpg");
 }
 
 function setup() {
-    createCanvas(800, 800);
+    createCanvas(600, 600);
 
     // Pass the JSON values into constructors
+    let space = 200;
     bubbles[0] = new bubble(wordHolder[0], 100, 100);
+    bubbles[1] = new bubble(wordHolder[1], 100 + space, 100);
+    bubbles[2] = new bubble(wordHolder[2], 100 + 2 * space, 100);
+    bubbles[3] = new bubble(wordHolder[3], 100, 250);
+    bubbles[4] = new bubble(wordHolder[4], 100 + space, 250);
+    bubbles[5] = new bubble(wordHolder[5], 100 + 2 * space, 250);
+    bubbles[6] = new bubble(wordHolder[6], 100, 400);
+    bubbles[7] = new bubble(wordHolder[7], 100 + space, 400);
+    bubbles[8] = new bubble(wordHolder[8], 100 + 2 * space, 400);
+
+    myChoice.start();
 }
 
 function draw() {
-    background(200);
+    if (choiceStage) {
+        background(200);
 
-    bubbles[0].display();
+        textAlign(CENTER);
+        textSize(18);
+        text("Read aloud three keywords that best describe your dream.", 300, 550)
+
+        // Display the bubbles
+        for (i = 0; i < 9; i++) {
+            if (!bubbles[i].selected) {
+                bubbles[i].display();
+            } else if (bubbles[i].selected) {
+                bubbles[i].display_selected();
+                counter++;
+            }
+        }
+        if (counter > 3) {
+            choiceStage = false;
+            counter = 0;
+        }
+
+    } else if (!choiceStage) {
+        background(200);
+        frameRate(20);
+        tint(255, tr);
+        image(dream, 0, 0);
+        tr++
+    }
+
+
 }
 
-// function parseResult() {
-//     let mostrecentword = myRec.resultString.split(' ').pop();
-//     if (mostrecentword.indexOf("left") !== -1) {
-//         dx = -1;
-//         dy = 0;
-//     } else if (mostrecentword.indexOf("right") !== -1) {
-//         dx = 1;
-//         dy = 0;
-//     } else if (mostrecentword.indexOf("up") !== -1) {
-//         dx = 0;
-//         dy = -1;
-//     } else if (mostrecentword.indexOf("down") !== -1) {
-//         dx = 0;
-//         dy = 1;
-//     } else if (mostrecentword.indexOf("clear") !== -1) {
-//         background(255);
-//     }
-//     console.log(mostrecentword);
-// }
+function parseResult() {
+    let mostrecentword = myChoice.resultString.split(' ').pop();
+    if (mostrecentword.indexOf("cloud") !== -1) {
+        bubbles[0].selected = true;
+    } else if (mostrecentword.indexOf("dream") !== -1) {
+        bubbles[1].selected = true;
+    } else if (mostrecentword.indexOf("catch") !== -1) {
+        bubbles[2].selected = true;
+    } else if (mostrecentword.indexOf("float") !== -1) {
+        bubbles[3].selected = true;
+    } else if (mostrecentword.indexOf("cable") !== -1) {
+        bubbles[4].selected = true;
+    } else if (mostrecentword.indexOf("spirit") !== -1) {
+        bubbles[5].selected = true;
+    } else if (mostrecentword.indexOf("sad") !== -1) {
+        bubbles[6].selected = true;
+    } else if (mostrecentword.indexOf("threat") !== -1) {
+        bubbles[7].selected = true;
+    } else if (mostrecentword.indexOf("pretty") !== -1) {
+        bubbles[8].selected = true;
+    } else if (mostrecentword.indexOf("hey") !== -1) {
+        console.log("hey");
+    }
+    console.log(mostrecentword);
+}
 
 // function visual() {
 
 // }
 
 // function choice() {
-    
+
 // }
